f5462c48cbd081f9257ca387868813ffd157cf2f
ciacono <claire.iacono@gmail.com>
2019-06-29 08:19:12 -0700

    delete and get request working with mongo

--- routes/api.js
+++ routes/api.js
@@ -1,17 +1,13 @@
 const express = require('express');
 const router = express.Router();
-const uuidv4 = require('uuid/v4');
 const Message = require('../models/message');
-
-//initial messages:
-var coolFunState = [{id: uuidv4(), text: 'Messages are great!'},
-        {id: uuidv4(), text: 'What is your favorite dog breed?'},
-        {id: uuidv4(), text: 'Water is wet.'}];
 
 //getting the list of messages
 router.get('/messages', function(req, res){
-    res.json(coolFunState);
-    console.log('GET request', req.body);
+    Message.find({}).then(function(messages){
+        res.send(messages);
+    });
+    console.log('GET request');
 });
 
 //adding a new message -> input task attached to req.body
@@ -20,17 +16,15 @@ router.post('/messages', function(req, res){
         res.json(message);
     }).catch(error => {console.log('caught', error.message);
     });
-    //TODO handle IDs
-    //console.log(JSON.stringify(req.body));
-    //coolFunState.push(req.body); //adding task to messages state array
     console.log('POST request', req.body);
 });
 
 //deleting list of messages
 router.delete('/messages', function(req, res){
-    coolFunState = [];
+    Message.deleteMany({}).then(function(messages){
+        res.send(messages);
+    }).catch(error => {console.log('caught', error.message)});
     console.log('DELETE request');
-    res.json(coolFunState);
 });
 
 module.exports = router;
