2c074261ac6d98819f66739888af46c75eb36c50
ciacono <claire.iacono@gmail.com>
2019-07-15 22:37:07 -0700

    fixed port hopefully to allow deployment

--- server.js
+++ server.js
@@ -4,7 +4,6 @@ const routes = require('./routes/api');
 const app = express();
 const cors = require('cors');
 const mongoose = require('mongoose');
-const port = process.env.port || 4000;
 require('dotenv').config();
 
 const path = require('path');
@@ -15,13 +14,11 @@ app.use('/api', routes);
 app.use(express.static(__dirname));
 app.use(express.static(path.join(__dirname, "client", "build")));
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/messagesdb');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/messagesdb',  { useNewUrlParser: true });
 mongoose.Promise = global.Promise;
 
 app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "client", "build", "server.js.html"));
 });
 
-app.listen(port, function(){
-    console.log('now listening for requests');
-});
+app.listen(process.env.PORT || 4000);
